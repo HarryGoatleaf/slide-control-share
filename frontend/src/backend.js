0f3b9718ed7e80774560f4faa5da3762fa9ed9df
HarryGoatleaf <peter.reithofer@gmx.net>
2022-11-18 17:13:12 +0100

    fix correct integration of .env

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const backend = axios.create({
     withCredentials: true,
-    baseURL: import.meta.env.BACKEND_URL + '/api',
+    baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
 })
